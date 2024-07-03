038ad4d30a2c5c8c7c24aec665decc40577c1fd7
aliza <ak6614462@gmail.com>
2024-07-03 14:54:58 +0500

    boilerplate

---
+++ boiler-plate/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Boiler Plate Assignment</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, dolorum.</p>
+        <img src="https://dictionary.cambridge.org/images/thumb/book_noun_001_01679.jpg?version=6.0.25" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
